0ce9837208fdeb09f0abf3b202398dd64a445b06
Ryan Hendrawan <ryanhendrawan9@gmail.com>
2025-06-15 20:22:03 +0800

    feat: Update CV

--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-4 text-center text-white bg-gray-900">
-      <p>
-        &copy; {new Date().getFullYear()} Ryan Hendrawan. All rights reserved.
-      </p>
+      <p>&copy; {new Date().getFullYear()} LaVine. All rights reserved.</p>
     </footer>
   );
 };
